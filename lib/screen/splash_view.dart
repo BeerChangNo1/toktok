bf8974385dfdaa8d52f7a2af781b457fcdbcebef
satawat miwan <satawatmiwan@gmail.com>
2023-06-19 15:27:58 +0700

    คถ

--- lib/screen/splash_view.dart
+++ lib/screen/splash_view.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 import 'package:toktok/addon/style.dart';
 import 'package:toktok/screen/hompage.dart';
 // import 'package:toktok/screen/login.dart';
-
+//ควยถอก
 class splash_view extends StatelessWidget {
   const splash_view({super.key});
 
